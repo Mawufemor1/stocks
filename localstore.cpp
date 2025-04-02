f03d3eb252082b77f504ec6d8eb9d30159a8ecdb
Mawufemor1 <mahiayibor001@st.ug.edu.gh>
2025-04-02 14:37:27 +0000

    Add files via upload

---
+++ localstore.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+ int main ()
+ { double originalprice , percentage, salestaxrate, mark_up_amount, sellingprice, salestax, finalprice ;
+
+    cout << " Enter the original price of the item sold. " << endl;
+    cin >> originalprice;
+
+    cout << " Enter percentage of the marked-up price. " << endl;
+    cin >> percentage;
+    
+
+    cout << " Enter sales tax rate. " << endl;
+    cin >> salestaxrate;
+
+    cout << " Origional price of the item = " << originalprice << endl;
+    cout << " Percentage of mark-up = " << percentage << endl;
+    
+    
+    mark_up_amount = originalprice * (percentage/100);
+    sellingprice = originalprice + mark_up_amount;
+    cout << "  Stores selling price of the item = " << sellingprice << endl;
+ 
+    
+    salestax = sellingprice * (salestaxrate/100);
+    cout << " Sales tax = " << salestax << endl;
+
+    finalprice = sellingprice + salestax;
+    cout << " Final price of the item = " << finalprice << endl;
+
+    
+
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+ }
